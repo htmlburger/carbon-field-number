260a33f24c8ed53f94fba40a8276cdefa706a01a
atanas-angelov-dev <atanas.angelov.dev@gmail.com>
2017-05-02 09:54:08 +0300

    remove plugin-style comments and fix unused constant

--- carbon-field-number.php
+++ carbon-field-number.php
@@ -1,17 +1,11 @@
 <?php
-/**
- * Plugin Name: Carbon Fields: Number
- * Description: A Carbon Fields extension, that adds a number field type.
- * Version: 1.0.0
- */
-
 define( 'Carbon_Field_Number\\DIR', __DIR__ );
 
 /**
  * Load field localization.
  */
-\Carbon_Fields\Carbon_Fields::on_boot( function() {
-	$dir = __DIR__ . '/languages/';
+Carbon_Fields\Carbon_Fields::on_boot( function() {
+	$dir = Carbon_Field_Number\DIR . '/languages/';
 	$locale = get_locale();
 	$path = $dir . $locale . '.mo';
 	load_textdomain( 'carbon-field-number', $path );
