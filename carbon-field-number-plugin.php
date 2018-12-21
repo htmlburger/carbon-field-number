0800e9cb71743d90e46600a7aa4e47b5a23f0315
ggeorgiev <ggeorgiev@htmlbox.com>
2018-12-21 12:42:13 +0200

    Update plugin file version and stats

--- carbon-field-number-plugin.php
+++ carbon-field-number-plugin.php
@@ -2,13 +2,13 @@
 /**
  * Plugin Name: Carbon Field Number
  * Description: Number Field extension for Carbon Fields.
- * Version: 2.0.0
+ * Version: 3.0.0
  * Author: htmlburger
  * Author URI: https://htmlburger.com/
  * Plugin URI: http://carbonfields.net/
  * License: GPL2
- * Requires at least: 4.0
- * Tested up to: 4.8
+ * Requires at least: 5.3
+ * Tested up to: 7.2.8
  * Text Domain: carbon-field-number
  * Domain Path: /languages
  */
