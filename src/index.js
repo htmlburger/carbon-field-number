938dd34e5f43844084b924f8caeb5d8dc24c09f4
ggeorgiev <ggeorgiev@htmlbox.com>
2018-12-21 12:43:58 +0200

    Bump 'carbon-fields' dependency version

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+ * External dependencies.
+ */
+import { registerFieldType } from '@carbon-fields/core';
+
+/**
+ * Internal dependencies.
+ */
+import NumberField from './main';
+
+registerFieldType( 'number', NumberField );
---
+++ src/main.js
@@ -0,0 +1,50 @@
+/**
+ * External dependencies.
+ */
+import { Component } from '@wordpress/element';
+
+class NumberField extends Component {
+	/**
+	 * Handles the change of the input.
+	 *
+	 * @param  {Object} e
+	 * @return {void}
+	 */
+	handleChange = ( e ) => {
+		const { id, onChange } = this.props;
+
+		onChange( id, e.target.value );
+	}
+
+	/**
+	 * Render a number input field.
+	 *
+	 * @return {Object}
+	 */
+	render() {
+		const {
+			id,
+			name,
+			value,
+			field
+		} = this.props;
+		const { handleChange } = this;
+
+		return (
+			<input
+				type="number"
+				id={id}
+				name={name}
+				value={value}
+				className="regular-text"
+				max={field.max}
+				min={field.min}
+				step={field.step}
+				onChange={this.handleChange} />
+		);
+	}
+}
+
+export default NumberField;
+
+
