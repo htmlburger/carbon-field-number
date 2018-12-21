d660724f590afbcc4f16fa07090e7058bc623481
ggeorgiev <ggeorgiev@htmlbox.com>
2018-12-21 12:40:25 +0200

    Remove old implementation assets

--- assets/js/bootstrap.js
+++
@@ -1,7 +0,0 @@
-/**
- * The internal dependencies.
- */
-import { registerFieldComponent } from 'lib/registry';
-import NumberField from 'components/field';
-
-registerFieldComponent('number', NumberField);
